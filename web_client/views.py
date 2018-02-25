b187eb79e8c9505a15568895be0c24e0923f22f2
Stan Reduta <stanislaw.reduta@gmail.com>
2018-02-25 16:11:16 +0100

    Adds base.html template

--- web_client/views.py
+++ web_client/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import View, TemplateView
 
-# Create your views here.
+
+class Homepage(TemplateView):
+    template_name = 'homepage/homepage_base_template.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
